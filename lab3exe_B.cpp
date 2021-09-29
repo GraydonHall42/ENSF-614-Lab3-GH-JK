a9e224d2d5c724b72a6d6f41af862f76c5db9679
GraydonHall42 <gwhall@ualberta.ca>
2021-09-28 22:17:33 -0600

    GH complete Ex A and B

--- lab3exe_B.cpp
+++ lab3exe_B.cpp
@@ -43,3 +43,47 @@ void global_print(const Cplx& n)
     << ", "<< n.getImaginaryPart( ) << ")" << endl;
     
 }
+
+// pasted from cplx_number.cpp
+Cplx::Cplx(double real , double imag ): realM(real),
+imagM(imag) {
+    // point three - When the program reaches this point for the second time
+}
+
+double Cplx::getRealPart() const {
+    // point two - When the program reaches this point for the first time
+    return realM;
+}
+
+double Cplx::getImaginaryPart( ) const {
+    return imagM;
+}
+
+void Cplx::setRealPart(double arg) {
+    
+    realM = arg;
+    // Point one
+}
+
+void Cplx::setImaginaryPart(double arg) {
+    imagM = arg;
+}
+
+Cplx Cplx::add(const Cplx& other)const {
+    Cplx local;
+    
+    local.realM = this->getRealPart() + other.realM;
+    local.imagM = imagM + other.imagM;
+    
+    return local;
+}
+
+Cplx Cplx::subtract(const Cplx* other)const {
+    Cplx local;
+    local.realM = realM - other->realM;
+    local.imagM = imagM - other->imagM;
+    
+    // point four
+    
+    return local;
+}
