43cdba94a3b52a65de4fbca9dd39625be91e94f2
GraydonHall42 <gwhall@ualberta.ca>
2021-09-30 20:20:58 -0600

    add final report and zip file to hand in

--- GH/tempCodeRunnerFile.cpp
+++ GH/tempCodeRunnerFile.cpp
@@ -1,2 +1,15 @@
+void DynString::append(const DynString& tail){
+  int new_length = lengthM + tail.lengthM;  // find new length
+  char * holder;  // char array to hold our new string
+  holder = new char[new_length];  // add 1 to length to account for '\0'
+  strcpy(holder, storageM);  // copy starageM contents into holder first
 
-            minute--;
+  // copy contents of tail into holder
+  for (int i = 0; i < tail.lengthM; i++) {
+    holder[i+lengthM] = tail.storageM[i];
+  }
+
+  holder[new_length] = '\0';
+  storageM = holder;
+  lengthM = new_length;
+}
